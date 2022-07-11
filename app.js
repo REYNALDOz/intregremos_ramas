8575a3d0de51a9e07109ba041ad60f3195858560
Reynaldo <reynaldo.quispe.27@unsch.edu.pe>
2022-07-11 14:51:53 -0500

    add: addserver112

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.send("<h1>HcUy6Re2LLBRtj</h1>");
+}); */
+
+app.get("/:nombre", (req, res) => {
+  if (req.params.nombre === '') {
+    res.send("<h1>Hola Desconocido</h1>");
+  }
+  res.send(`<h1>Hola ${req.params.nombre}</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
